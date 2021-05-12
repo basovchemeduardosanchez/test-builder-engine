838c83f3a0ca4e4e97f0d14e27b679f00ac20a10
Eduardo Sánchez Villafán <basov_chem_eduardo_sanchez@hotmail.com>
2021-05-11 21:23:36 -0500

    chore: identify pending tasks

--- src/init.js
+++ src/init.js
@@ -1,4 +1,6 @@
 ﻿var init = { 
+	/* TODO: The following object should be replaced by the report engine integration so the data is pulled from a REST service */
+	/* TODO: The data population on the table will be performed from the form engine */
      'questions': [ 
        {
            'question': 'jQuery is a...',
@@ -15,16 +17,17 @@
            'answers': ['NULL','Sampling Shell','a bash command','secure shell'],
 			  'correctAnswer': 4
        }
-     ],
+	 ],
+	 /* TODO: The following settings should be pulled from the report engine connected to a table in order to provide the user with and interface to select data to be deployed in a pseudo query format */
 	  'resultComments' :  
 	  {
 		    perfect: 'Great Job!!',
-			 excellent: 'Outstanding!',
-			 good: 'Acceptable, requires additional training.',
-			 average: 'Your performance is borderline.',
-			 bad: 'Your performance is bad requires review and proper training.',
-			 poor: 'Your result is unacceptable',
-			 worst: 'Not applicable fo further evaluations'
+			excellent: 'Outstanding!',
+			good: 'Acceptable, requires additional training.',
+			average: 'Your performance is borderline.',
+			bad: 'Your performance is bad requires review and proper training.',
+			poor: 'Your result is unacceptable',
+			worst: 'Not applicable fo further evaluations'
 	  }
 
  };
